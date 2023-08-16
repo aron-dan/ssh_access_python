828bc1b4c46fe64ced4f6cba8312b04e4c4a21e0
aron-dan <82916233+aron-dan@users.noreply.github.com>
2023-08-16 00:15:20 -0300

    feat: corrigido o acesso SSH

--- AccesSSH.py
+++ AccesSSH.py
@@ -1,26 +1,27 @@
-from paramiko import SSHClient
 import paramiko
 
+hostname = "192.168.1.150"
+username = "coletivo"
+password = "colR22@Acx04D33"
+port = 6422
 
 
-def __init__(self):
-    self.ssh = SSHClient()
-
+with paramiko.SSHClient() as client:
     # Instrução para ler chaves cadastros para evitar clicar em yes ou no
-    self.ssh.load_system_host_keys()
+    client.load_system_host_keys()
 
-    # Para caso a chave do servidor não for conhecida
-    self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # Para caso a chave do servidor não for conhecida, priemira tentativa
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    self.ssh.connect(hostname='127.0.0.1', username='root', password='SENHA_DE_ROOT')
+    # Connect to the SSH server
+    client.connect(hostname, port, username, password, look_for_keys=False)
+    print("ok")
+    (stdin, stdout, stderr) = client.exec_command("ip address print")
 
-def exec_cmd(self,cmd):
-    stdin,stdout,stderr = self.ssh.exec_command(cmd)
-    if stderr.channel.recv_exit_status() != 0:
-        print (stderr.read())
-    else:
-        print (stdout.read())
+    output = stdout.read()
+    print(str(output, 'utf8'))
 
-if __name__ == '__main__':
-    ssh = SSH()
-    ssh.exec_cmd("apt-get update")
+    client.close()
+
+
+
